2641f8a1a931b5a8d1a50aec41c21797e107f9ea
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-12 08:19:22 -0300

    Revert "fully implemented the User clas"
    This reverts commit 900c614546651171b34acd198bab9c92ffb8499e.

--- src/model/User.java
+++ src/model/User.java
@@ -1,57 +1,5 @@
 package model;
 
-import java.util.Objects;
-
 public class User {
-	private String email;
-	private String password;
-	private int id;
 
-	public User(String email, String password, int id) {
-		this.email = email;
-		this.password = password;
-		this.id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (o == null || getClass() != o.getClass())
-			return false;
-		User user = (User) o;
-		return id == user.id && Objects.equals(email, user.email) && Objects.equals(password, user.password);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(email, password, id);
-	}
-
-	@Override
-	public String toString() {
-		return "User{" + "email='" + email + '\'' + ", password='" + password + '\'' + ", id=" + id + '}';
-	}
 }
